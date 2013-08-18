b9328223635140b9457d0c14eecf536e94f83ff7
Zarick Lau <zarick@gmail.com>
2013-08-18 23:36:08 +0800

    Initial Commit

---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Lau Hok Yan on 18/8/13.
+//  Copyright (c) 2013 Zarick's Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
