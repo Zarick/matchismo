79cc1c30b9fafe58beac9f285cfdc126166b7c08
Zarick Lau <zarick@gmail.com>
2013-08-31 22:26:47 +0800

    Created view for Set game

---
+++ Matchismo/SetGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetGameViewController.h
+//  Matchismo
+//
+//  Created by Zarick on 31/8/13.
+//  Copyright (c) 2013 Zarick's Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetGameViewController : UIViewController
+
+@end
---
+++ Matchismo/SetGameViewController.m
@@ -0,0 +1,38 @@
+//
+//  SetGameViewController.m
+//  Matchismo
+//
+//  Created by Zarick on 31/8/13.
+//  Copyright (c) 2013 Zarick's Lab. All rights reserved.
+//
+
+#import "SetGameViewController.h"
+
+@interface SetGameViewController ()
+
+@end
+
+@implementation SetGameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
