a5b0b4d4f23f3917338a8c7c95d4a63e7b2e0b0c
Zarick Lau <zarick@gmail.com>
2013-08-31 22:49:39 +0800

    Wire up the outlet and action

--- Matchismo/SetGameViewController.m
+++ Matchismo/SetGameViewController.m
@@ -1,9 +1,26 @@
 #import "SetGameViewController.h"
+#import "CardMatchingGame.h"
+
 
 @interface SetGameViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *flipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *gameStatusLabel;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+
+@property (nonatomic) CardMatchingGame *game;
+
 @end
 
+
 @implementation SetGameViewController
+
+- (void)updateUI {
+    // ● ■ ▲    
+}
+
+- (IBAction)deal {
+}
 
 @end
