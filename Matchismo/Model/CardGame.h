35266fb84161e192b8cb165d918dd53d4b5db2eb
Zarick Lau <zarick@gmail.com>
2013-09-01 19:21:40 +0800

    Extract CardGame as super class of PokerMatchCardGame

---
+++ Matchismo/Model/CardGame.h
@@ -0,0 +1,19 @@
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface CardGame : NSObject
+
+- (id)initWithCardCount:(NSUInteger)cardCount
+               fromDeck:(Deck *)deck
+          shouldShuffle:(BOOL)shouldShuffle
+               matching:(NSUInteger)totalFlip;
+
+- (void)flipCardAtIndex:(NSUInteger)index;
+
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic, readonly) int score;
+@property (strong, nonatomic, readonly) NSString *status;
+
+
+@end
--- Matchismo/Model/PokerMatchCardGame.h
+++ Matchismo/Model/PokerMatchCardGame.h
@@ -1,18 +1,6 @@
 #import <Foundation/Foundation.h>
-#import "Deck.h"
+#import "CardGame.h"
 
-@interface PokerMatchCardGame : NSObject
-
-- (id)initWithCardCount:(NSUInteger)cardCount
-               fromDeck:(Deck *)deck
-               shouldShuffle:(BOOL)shouldShuffle
-               matching:(NSUInteger)totalFlip;
-
-- (void)flipCardAtIndex:(NSUInteger)index;
-
-- (Card *)cardAtIndex:(NSUInteger)index;
-
-@property (nonatomic, readonly) int score;
-@property (strong, nonatomic, readonly) NSString *status;
+@interface PokerMatchCardGame : CardGame
 
 @end
