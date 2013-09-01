e526ce028bf7a23d5b398087d28886d0859219c0
Zarick Lau <zarick@gmail.com>
2013-09-01 19:10:56 +0800

    Cleanup comments

--- Matchismo/Model/PlayingCardDeck.m
+++ Matchismo/Model/PlayingCardDeck.m
@@ -1,11 +1,3 @@
-//
-//  PlayingCardDeck.m
-//  Matchismo
-//
-//  Created by Zarick on 24/8/13.
-//  Copyright (c) 2013 Zarick's Lab. All rights reserved.
-//
-
 #import "PlayingCardDeck.h"
 #import "PlayingCard.h"
 
